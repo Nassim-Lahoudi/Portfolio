92a5b73d746fd4b2751d954033e22eaccde6acd6
Naxxim <Nassim.Lahoudi@hotmail.com>
2025-03-19 14:48:47 +0100

    Upload New Files

---
+++ script.js
@@ -0,0 +1,5 @@
+function emailFunction() {
+    let e_mail = "nassim.lahoudi@hotmail.com"
+    let github = "https://github.com/Nassim-Lahoudi"
+    alert("E-Mail: " + e_mail + "\nGitHub Link: " + github)
+}
