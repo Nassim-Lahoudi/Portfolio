6ed05ffe43cfbb772baf5b0bda232e73a8068472
Nassim Lahoudi <97837009+Nassim-Lahoudi@users.noreply.github.com>
2025-10-13 20:20:42 +0000

    Feat: Update Vite configuration to include build output directory

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.', // oder 'src', wenn deine index.html in src/ liegt
+  build: {
+    outDir: 'dist',
+  },
 })
